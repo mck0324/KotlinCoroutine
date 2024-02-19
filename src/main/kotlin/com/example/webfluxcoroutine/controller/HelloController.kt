0cc546c1c723b63cbdfa86633d617269b2052aa7
mck0324 <dch3030@naver.com>
2024-02-19 22:18:11 +0900

    Kotest적용해서 Repository 테스트해보기

---
+++ src/main/kotlin/com/example/webfluxcoroutine/controller/HelloController.kt
@@ -0,0 +1,20 @@
+package com.example.webfluxcoroutine.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    suspend fun index(): String {
+        return "main page"
+    }
+
+    @GetMapping("/hello/{name}")
+    suspend fun hello(@PathVariable name: String): String {
+        return "Hello $name"
+    }
+}
--- src/test/kotlin/com/example/webfluxcoroutine/WebfluxCoroutineApplicationTests.kt
+++ src/test/kotlin/com/example/webfluxcoroutine/WebfluxCoroutineApplicationTests.kt
@@ -1,13 +1,25 @@
 package com.example.webfluxcoroutine
 
+import com.example.webfluxcoroutine.model.Article
+import com.example.webfluxcoroutine.repository.ArticleRepository
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class WebfluxCoroutineApplicationTests {
-
-    @Test
-    fun contextLoads() {
+class WebfluxCoroutineApplicationTests(
+    @Autowired private val repository: ArticleRepository,
+): StringSpec ({
+    "context load".config(false) {
+        val prev = repository.count()
+        repository.save(Article(title = "hihi"))
+        val curr = repository.count()
+        curr shouldBe prev + 1
+//        Assertions.assertEquals(prev+1,curr)
     }
+})
 
-}
